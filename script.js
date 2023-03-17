caa519cf73b363d5f5dc76f22cf039bf69b49840
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-03-17 20:13:05 +0100

    Added book table and add new book button

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const myLibrary = [];
+// New book button with form for book details and submit button
+// 4. Add details input
+// 5. Make input into an object
+// 6. Push object to array (original one, and delete books array)
+
+const table = document.querySelector('tbody');
+const formContainer = document.querySelector('#formContainer');
+const formBtn = document.querySelector('button');
 
 // Constructor function
 function Book(title, author, pages, isRead) {
@@ -9,34 +16,75 @@ function Book(title, author, pages, isRead) {
   this.info = () => `${title} by ${author}, ${pages}, ${isRead}`;
 }
 
-// Create new instances of the Book object using it's constructor function
-const theHobbit = new Book(
-  'The Hobbit',
-  'J.R.R. Tolkien',
-  '295 pages',
-  'not read yet'
-);
+const myLibrary = [
+  new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet'),
+  new Book('The Enchiridion', 'Epictetus', '28 pages', 'is read'),
+  new Book('The Prince', 'Niccolo Machiavelli', '144 pages', 'not read yet'),
+];
 
-const theEnchiridion = new Book(
-  'The Enchiridion',
-  'Epictetus',
-  '28 pages',
-  'is read'
-);
+// function appendBook(newBook) {
+//   const tableRow = document.createElement('tr');
 
-const thePrince = new Book(
-  'The Prince',
-  'Niccolo Machiavelli',
-  '144 pages',
-  'not read yet'
-);
+//   const titleCell = document.createElement('td');
+//   titleCell.textContent = newBook.title;
+//   tableRow.appendChild(titleCell);
+//   table.appendChild(tableRow);
+// }
 
-console.log(theHobbit.info(), theEnchiridion.info(), thePrince.info());
+function openForm() {
+  const form = document.createElement('form');
 
-function addBookToLibrary() {
-  myLibrary.push(theHobbit, theEnchiridion, thePrince);
+  // Create form elements
+  const titleLabel = document.createElement('label');
+  titleLabel.textContent = 'Title: ';
+  const titleInput = document.createElement('input');
+  titleInput.type = 'text';
+  titleLabel.appendChild(titleInput);
+  form.appendChild(titleLabel);
+
+  // Add a submit button to the form
+  const submitButton = document.createElement('button');
+  submitButton.type = 'submit';
+  submitButton.textContent = 'Submit';
+  form.appendChild(submitButton);
+  formContainer.appendChild(form);
 }
 
-addBookToLibrary();
+function addBookToLibrary() {
+  openForm();
+  // const bookInput = prompt('Book: ');
+  // const newBook = new Book(bookInput);
+  // myLibrary.push(newBook);
+  // appendBook(newBook);
+  // myLibrary.push(...books);
+}
+
+formBtn.addEventListener('click', addBookToLibrary);
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    const tableRow = document.createElement('tr');
+
+    const titleCell = document.createElement('td');
+    titleCell.textContent = book.title;
+    tableRow.appendChild(titleCell);
+
+    const authorCell = document.createElement('td');
+    authorCell.textContent = book.author;
+    tableRow.appendChild(authorCell);
+
+    const pagesCell = document.createElement('td');
+    pagesCell.textContent = book.pages;
+    tableRow.appendChild(pagesCell);
+
+    const isReadCell = document.createElement('td');
+    isReadCell.textContent = book.isRead;
+    tableRow.appendChild(isReadCell);
+
+    table.appendChild(tableRow);
+  });
+}
+
+displayBooks();
 
 console.log(myLibrary);
